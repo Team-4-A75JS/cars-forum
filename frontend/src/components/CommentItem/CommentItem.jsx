8e4867f75afba389c181de0cb694770b68a17be4
Kaloyan Kissiov <kaloyank25@gmail.com>
2026-03-02 15:05:25 +0200

    add: "Downvote functionality and needed performance changes"

--- frontend/src/components/CommentItem/CommentItem.jsx
+++ frontend/src/components/CommentItem/CommentItem.jsx
@@ -1,23 +1,10 @@
 import "./CommentItem.css";
-import { getPrimaryBadge } from "../../utils/reputation";
 
 function CommentItem({ comment, canDelete = false, onDelete }) {
-    const primaryBadge = getPrimaryBadge({
-        reputation: comment.authorReputation,
-        postsCount: comment.authorPostsCount,
-        commentsCount: comment.authorCommentsCount,
-        createdAt: comment.authorCreatedAt,
-    });
-
     return (
         <div className="comment-item">
             <p className="comment-author-line">
                 <strong>{comment.author}</strong>
-                {primaryBadge && (
-                    <span className={`badge-chip badge-${primaryBadge.color}`}>
-                        {primaryBadge.label}
-                    </span>
-                )}
             </p>
             <p>{comment.text}</p>
             {canDelete && (
